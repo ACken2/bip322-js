bb4cabd6a16e3ee656456161edcaee32d01b7e2a
Ken Sze <acken2@outlook.com>
2023-06-24 17:51:02 +0800

    Implemented Signer

---
+++ src/Signer.ts
@@ -0,0 +1,110 @@
+// Import dependencies
+import BIP322 from "./BIP322";
+import ECPairFactory from 'ecpair';
+import { Address } from "./helpers";
+import * as bitcoin from 'bitcoinjs-lib';
+import ecc from '@bitcoinerlab/secp256k1';
+import * as bitcoinMessage from 'bitcoinjs-message';
+
+/**
+ * Class that signs BIP-322 signature using a private key.
+ * Reference: https://github.com/LegReq/bip0322-signatures/blob/master/BIP0322_signing.ipynb
+ */
+class Signer {
+
+    /**
+     * Sign a BIP-322 signature from P2WPKH, P2SH-P2WPKH, and single-key-spend P2TR address and its corresponding private key.
+     * @param privateKey Private key used to sign the message
+     * @param address Address to be signing the message
+     * @param message message_challenge to be signed by the address 
+     * @returns BIP-322 simple signature, encoded in base-64
+     */
+    public static sign(privateKey: string, address: string, message: string) {
+        // Initialize private key used to sign the transaction
+        const ECPair = ECPairFactory(ecc);
+        let signer = ECPair.fromWIF(privateKey);
+        // Check if the private key can sign message for the given address
+        if (!this.checkPubKeyCorrespondToAddress(signer.publicKey, address)) {
+            throw new Error(`Invalid private key provided for signing message for ${address}.`);
+        }
+        // Handle legacy P2PKH signature
+        if (Address.isP2PKH(address)) {
+            // For P2PKH address, sign a legacy signature
+            // Reference: https://github.com/bitcoinjs/bitcoinjs-message/blob/c43430f4c03c292c719e7801e425d887cbdf7464/README.md?plain=1#L21
+            return bitcoinMessage.sign(message, signer.privateKey, signer.compressed);
+        }
+        // Convert address into corresponding script pubkey
+        const scriptPubKey = Address.convertAdressToScriptPubkey(address);
+        // Draft corresponding toSpend using the message and script pubkey
+        const toSpendTx = BIP322.buildToSpendTx(message, scriptPubKey);
+        // Draft corresponding toSign transaction based on the address type
+        let toSignTx: bitcoin.Psbt;
+        if (Address.isP2SH(address)) {
+            // P2SH-P2WPKH signing path
+            // Derive the P2SH-P2WPKH redeemScript from the corresponding hashed public key
+            const redeemScript = bitcoin.payments.p2wpkh({
+                hash: bitcoin.crypto.hash160(signer.publicKey)
+            }).output as Buffer;
+            toSignTx = BIP322.buildToSignTx(toSpendTx.getId(), redeemScript, true);
+        }
+        else if (Address.isP2WPKH(address)) {
+            // P2WPKH signing path
+            toSignTx = BIP322.buildToSignTx(toSpendTx.getId(), scriptPubKey);
+        }
+        else {
+            // P2TR signing path
+            // Extract the taproot internal public key
+            const internalPublicKey = signer.publicKey.subarray(1, 33);
+            // Tweak the private key for signing, since the output and address uses tweaked key
+            // Reference: https://github.com/bitcoinjs/bitcoinjs-lib/blob/1a9119b53bcea4b83a6aa8b948f0e6370209b1b4/test/integration/taproot.spec.ts#L55
+            signer = signer.tweak(
+                bitcoin.crypto.taggedHash('TapTweak', signer.publicKey.subarray(1, 33))
+            );
+            // Obtain the script public key
+            const scriptPubKey = bitcoin.payments.p2tr({
+                address: address
+            }).output as Buffer;
+            // Draft a toSign transaction that spends toSpend transaction
+            toSignTx = BIP322.buildToSignTx(toSpendTx.getId(), scriptPubKey, false, internalPublicKey);
+            // Set the sighashType to bitcoin.Transaction.SIGHASH_ALL since it defaults to SIGHASH_DEFAULT
+            toSignTx.updateInput(0, {
+                sighashType: bitcoin.Transaction.SIGHASH_ALL
+            });
+        }
+        // Sign the toSign transaction
+        const toSignTxSigned = toSignTx.signAllInputs(signer, [bitcoin.Transaction.SIGHASH_ALL]).finalizeAllInputs();
+        // Extract and return the signature
+        return BIP322.encodeWitness(toSignTxSigned);
+    }
+
+    /**
+     * Check if a given public key is the public key for a claimed address.
+     * @param publicKey Public key to be tested
+     * @param claimedAddress Address claimed to be derived based on the provided public key
+     * @returns True if the claimedAddress can be derived by the provided publicKey, false if otherwise
+     */
+    private static checkPubKeyCorrespondToAddress(publicKey: Buffer, claimedAddress: string) {
+        // Derive the same address type from the provided public key
+        let derivedAddresses: { mainnet: string, testnet: string };
+        if (Address.isP2PKH(claimedAddress)) {
+            derivedAddresses = Address.convertPubKeyIntoAddress(publicKey, 'p2pkh');
+        }
+        else if (Address.isP2SH(claimedAddress)) {
+            derivedAddresses = Address.convertPubKeyIntoAddress(publicKey, 'p2sh-p2wpkh');
+        }
+        else if (Address.isP2WPKH(claimedAddress)) {
+            derivedAddresses = Address.convertPubKeyIntoAddress(publicKey, 'p2wpkh');
+        }
+        else if (Address.isP2TR(claimedAddress)) {
+            derivedAddresses = Address.convertPubKeyIntoAddress(publicKey, 'p2tr');
+        }
+        else {
+            throw new Error('Unable to sign BIP-322 message for unsupported address type.'); // Unsupported address type
+        }
+        // Check if the derived address correspond to the claimedAddress
+        return (derivedAddresses.mainnet === claimedAddress) || (derivedAddresses.testnet === claimedAddress);
+    }
+
+}
+
+export default Signer;
---
+++ test/Signer.test.ts
@@ -0,0 +1,102 @@
+// Import dependencies
+import { expect } from 'chai';
+import * as bitcoinMessage from 'bitcoinjs-message';
+
+// Import module to be tested
+import Signer from '../src/Signer';
+
+describe('Signer Test', () => {
+
+    it('Can sign legacy P2PKH signature', () => {
+        // Arrange
+        const privateKey = 'L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k';
+        const address = '14vV3aCHBeStb5bkenkNHbe2YAFinYdXgc';
+        const message = 'Hello World';
+
+        // Act
+        const signature = Signer.sign(privateKey, address, message);
+
+        // Assert
+        expect(bitcoinMessage.verify(message, address, signature)).to.be.true;
+    });
+
+    it('Can sign BIP-322 signature using nested segwit address', () => {
+        // Arrange
+        const privateKey = 'KwTbAxmBXjoZM3bzbXixEr9nxLhyYSM4vp2swet58i19bw9sqk5z'; // Private key of "3HSVzEhCFuH9Z3wvoWTexy7BMVVp3PjS6f"
+        const address = '3HSVzEhCFuH9Z3wvoWTexy7BMVVp3PjS6f';
+        const message = 'Hello World';
+        const expectedSignature = 'AkgwRQIhAMd2wZSY3x0V9Kr/NClochoTXcgDaGl3OObOR17yx3QQAiBVWxqNSS+CKen7bmJTG6YfJjsggQ4Fa2RHKgBKrdQQ+gEhAxa5UDdQCHSQHfKQv14ybcYm1C9y6b12xAuukWzSnS+w';
+
+        // Act
+        const signature = Signer.sign(privateKey, address, message);
+
+        // Assert
+        expect(signature).to.equal(expectedSignature);
+    });
+
+    it('Can sign BIP-322 signature using native segwit address', () => {
+        // Arrange
+        // Test vector listed at https://github.com/bitcoin/bitcoin/blob/29b28d07fa958b89e1c7916fda5d8654474cf495/src/test/util_tests.cpp#L2713
+        const privateKey = 'L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k';
+        const address = 'bc1q9vza2e8x573nczrlzms0wvx3gsqjx7vavgkx0l';
+        const message = 'Hello World';
+        const expectedSignature = 'AkgwRQIhAOzyynlqt93lOKJr+wmmxIens//zPzl9tqIOua93wO6MAiBi5n5EyAcPScOjf1lAqIUIQtr3zKNeavYabHyR8eGhowEhAsfxIAMZZEKUPYWI4BruhAQjzFT8FSFSajuFwrDL1Yhy';
+
+        // Act
+        const signature = Signer.sign(privateKey, address, message);
+
+        // Assert
+        expect(signature).to.equal(expectedSignature);
+    });
+
+    it('Can sign BIP-322 using single-key-spend taproot address', () => {
+        // Arrange
+        // Test vector listed at https://github.com/bitcoin/bitcoin/blob/29b28d07fa958b89e1c7916fda5d8654474cf495/src/test/util_tests.cpp#L2747
+        const privateKey = 'L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k';
+        const address = "bc1ppv609nr0vr25u07u95waq5lucwfm6tde4nydujnu8npg4q75mr5sxq8lt3";
+        const message = "Hello World";
+        const expectedSignature = "AUHd69PrJQEv+oKTfZ8l+WROBHuy9HKrbFCJu7U1iK2iiEy1vMU5EfMtjc+VSHM7aU0SDbak5IUZRVno2P5mjSafAQ==";
+
+        // Act
+        const signature = Signer.sign(privateKey, address, message);
+
+        // Assert
+        expect(signature).to.equal(expectedSignature);
+    });
+
+    it('Throw when the provided private key cannot derive the given signing address', () => {
+        // Arrange
+        const privateKey = 'L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k';
+        const p2pkhAddressWrong = '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2';
+        const p2shAddressWrong = '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy';
+        const p2wpkhAddressWrong = 'bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq';
+        const p2trAddressWrong = 'bc1p5d7rjq7g6rdk2yhzks9smlaqtedr4dekq08ge8ztwac72sfr9rusxg3297';
+        const message = 'Hello World';
+
+        // Act
+        const signP2PKH = Signer.sign.bind(Signer, privateKey, p2pkhAddressWrong, message);
+        const signP2SH = Signer.sign.bind(Signer, privateKey, p2shAddressWrong, message);
+        const signP2WPKH = Signer.sign.bind(Signer, privateKey, p2wpkhAddressWrong, message);
+        const signP2TR = Signer.sign.bind(Signer, privateKey, p2trAddressWrong, message);
+
+        // Assert
+        expect(signP2PKH).to.throws(`Invalid private key provided for signing message for ${p2pkhAddressWrong}.`)
+        expect(signP2SH).to.throws(`Invalid private key provided for signing message for ${p2shAddressWrong}.`)
+        expect(signP2WPKH).to.throws(`Invalid private key provided for signing message for ${p2wpkhAddressWrong}.`)
+        expect(signP2TR).to.throws(`Invalid private key provided for signing message for ${p2trAddressWrong}.`)
+    });
+
+    it('Throw when attempting to sign BIP-322 signature using unsupported address type', () => {
+        // Arrange
+        const privateKey = 'L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k';
+        const p2wshAddress = 'bc1qeklep85ntjz4605drds6aww9u0qr46qzrv5xswd35uhjuj8ahfcqgf6hak';
+        const message = 'Hello World';
+
+        // Act
+        const signP2WSH = Signer.sign.bind(Signer, privateKey, p2wshAddress, message);
+
+        // Assert
+        expect(signP2WSH).to.throws('Unable to sign BIP-322 message for unsupported address type.');
+    });
+
+});
