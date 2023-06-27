79028578dab5a5ad507a14b92b7b60b0592e54f1
Ken Sze <acken2@outlook.com>
2023-06-27 09:20:23 +0800

    Added index.ts for export

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Import modules to be exported
+import BIP322 from "./BIP322";
+import Signer from "./Signer";
+import Verifier from "./Verifier";
+import { Witness, Address } from "./helpers";
+
+// Export
+export { BIP322, Signer, Verifier, Witness, Address };
