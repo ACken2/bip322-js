0d5677882e78a82f5c895addc87222f6477eefe9
Ken Sze <acken2@outlook.com>
2023-08-19 22:40:47 +0800

    Added BIP137 helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,7 @@
 import Address from "./Address";
+import BIP137 from "./BIP137";
 import VarInt from "./VarInt";
 import VarStr from "./VarStr";
 import Witness from "./Witness";
 
-export { Address, VarInt, VarStr, Witness };
+export { Address, BIP137, VarInt, VarStr, Witness };
