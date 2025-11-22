9aa8562fc699a783a4a807a8329b866d6ad4dfd8
Ken Sze <acken2@outlook.com>
2025-11-22 17:07:36 +0800

    Added BitcoinMessage in export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,10 @@
 import Address from "./Address";
 import BIP137 from "./BIP137";
+import BitcoinMessage from "./BitcoinMessage";
 import BufferUtil from "./BufferUtil";
 import Key from "./Key";
 import VarInt from "./VarInt";
 import VarStr from "./VarStr";
 import Witness from "./Witness";
 
-export { Address, BIP137, BufferUtil, Key, VarInt, VarStr, Witness };
+export { Address, BitcoinMessage, BIP137, BufferUtil, Key, VarInt, VarStr, Witness };
